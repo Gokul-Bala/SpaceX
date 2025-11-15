45f0e8d30f54fbcde9b0b429342acbd8581e7f46
Gokul Bala <dhilipradancoc@gmail.com>
2025-11-15 15:15:26 +0000

    Remove GenAi it doesn't work

--- src/ai/dev.ts
+++
@@ -1,4 +0,0 @@
-// This file is for local development of Genkit flows.
-// It is not used in production.
-
-import '@/.next/server/app/api/genkit/[...slug]/route';
--- src/app/api/genkit/[...slug]/route.ts
+++
@@ -1,8 +0,0 @@
-
-import {createApiHandler} from '@genkit-ai/next';
-import {ai} from '@/ai/genkit';
-
-// This imports your flows for their side effects.
-import '@/ai/flows';
-
-export const {GET, POST} = createApiHandler(ai);
